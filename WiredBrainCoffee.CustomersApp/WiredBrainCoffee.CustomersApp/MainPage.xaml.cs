16933186e943990db7ad60254de0cf67b8aa6e39
ahmed <ahmed.sherif.fci@gmail.com>
2020-08-10 13:49:38 +0200

    access attached property using Static methods

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -25,10 +25,10 @@ namespace WiredBrainCoffee.CustomersApp
 
         private void ButtonMove_Click(object sender, RoutedEventArgs e)
         {
-            var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            var column = Grid.GetColumn(customerListGrid);
             var newColumn = column == 0 ? 2 : 0;
 
-            customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            Grid.SetColumn(customerListGrid, newColumn);
             moveSymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
         }
     }
